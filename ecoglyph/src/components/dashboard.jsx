f0b340fd77a9fc6f70d5ae92ac65f5ee55ba1219
Archit Shukla <asharchit03@gmail.com>
2024-05-02 13:31:34 +0530

    added css ti dashboard page and added global font file

--- ecoglyph/src/components/dashboard.jsx
+++ ecoglyph/src/components/dashboard.jsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
+import "./dashboard.css";
+import "../font.css";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="dashboard_main corben-regular">
       <h1>Ecoglyph DashBoard</h1>
-      <Link to="/qrgenerator">Qr generator</Link>
+      <div className="dashboard_card">
+        <Link to="/qrgenerator">
+          <div className="dashboard_card_text">Qr generator</div>
+        </Link>
+      </div>
+      <div className="dashboard_card">
+        <Link to="/qrgenerator">
+          <div className="dashboard_card_text">Qr generator</div>
+        </Link>
+      </div>
     </div>
   );
 }
